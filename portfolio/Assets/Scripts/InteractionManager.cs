be96f5309a68f1c0d134b3305afd26508bc99a36
Tom <prot.tom91@gmail.com>
2025-03-10 01:38:45 +0100

    camera movement complete

--- portfolio/Assets/Scripts/InteractionManager.cs
+++ portfolio/Assets/Scripts/InteractionManager.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Cinemachine;
 
 public class InteractionManager : MonoBehaviour
 {
-    private Camera cam;
+    private Camera cameraAnimation;
     private bool IsActive;
     private InteractMap inputActions;
     public event EventHandler<onSelectChangeEventArgs> onSelectChange;
+
+    [SerializeField] private GameObject camera;
+    [SerializeField] private GameObject cameraPrincipale;
+    [SerializeField] private GameObject cameraBibliothèque;
 
     public class onSelectChangeEventArgs : EventArgs
     {
@@ -16,21 +21,29 @@ public class InteractionManager : MonoBehaviour
     }
     [SerializeField] private LayerMask layer;
     private Transform lastSelected = null;
-
+    private Camera activeCamera;
+    public Vector3 mousePos;
     private void Awake()
     {
         inputActions = new InteractMap();
         inputActions.Enable();
         inputActions.InteractionMap.Interact.performed += Interact_performed;
-        cam = Camera.main;
+        cameraAnimation = Camera.main;
     }
 
     private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         if(lastSelected != null)
         {
-            lastSelected.GetComponent<LightSelected>().enabled = false;
+            if (lastSelected.CompareTag("Bibli"))
+            {
+                SwitchToEtagere();
+            }
             lastSelected = null;
+            onSelectChange?.Invoke(this, new onSelectChangeEventArgs
+            {
+                selection = lastSelected
+            });
         }
     }
 
@@ -38,9 +51,9 @@ public class InteractionManager : MonoBehaviour
     {
         if (IsActive)
         {
-            Vector3 mousePos = Input.mousePosition;
+            mousePos = Input.mousePosition;
             mousePos.z = 10f;
-            mousePos = cam.ScreenToWorldPoint(mousePos);
+            mousePos = cameraAnimation.ScreenToWorldPoint(mousePos);
             Physics.Raycast(transform.position, mousePos - transform.position, out RaycastHit raycast, 15f, layer); ;
             if (raycast.transform != lastSelected)
             {
@@ -63,5 +76,31 @@ public class InteractionManager : MonoBehaviour
     public void SetState(bool value)
     {
         IsActive = value;
+        SwitchCamera();
+    }
+    private void SwitchCamera()
+    {
+        camera.SetActive(true);
+        cameraAnimation.GetComponent<Animator>().enabled = false;
+    }
+    public void SwitchToEtagere()
+    {
+        cameraPrincipale.GetComponent<CinemachineVirtualCamera>().Priority = 0;
+        cameraBibliothèque.GetComponent<CinemachineVirtualCamera>().Priority = 1;
+        layer = LayerMask.GetMask("Projet");
+        cameraAnimation.transform.position = cameraBibliothèque.transform.position;
+        transform.position = cameraBibliothèque.transform.position;
+        transform.eulerAngles = cameraBibliothèque.transform.eulerAngles;
+        cameraAnimation.transform.eulerAngles = cameraBibliothèque.transform.eulerAngles;
+    }
+    public void SwitchToPrincipale()
+    {
+        cameraPrincipale.GetComponent<CinemachineVirtualCamera>().Priority = 1;
+        cameraBibliothèque.GetComponent<CinemachineVirtualCamera>().Priority = 0;
+        layer = LayerMask.GetMask("Interactible");
+        cameraAnimation.transform.position = cameraPrincipale.transform.position;
+        cameraAnimation.transform.eulerAngles = cameraPrincipale.transform.eulerAngles;
+        transform.position = cameraPrincipale.transform.position;
+        transform.eulerAngles = cameraPrincipale.transform.eulerAngles;
     }
 }
